5c6fb43d66b108b886b17652389928dc6a29eb54
Venkatesh <venkatesh@stockgro.com>
2023-08-16 14:42:28 +0530

    events added

--- feature/home/src/main/java/com/example/home/presentation/viewmodel/HomeViewModel.kt
+++ feature/home/src/main/java/com/example/home/presentation/viewmodel/HomeViewModel.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.home.domain.usecase.GetUserUseCase
-import com.example.home.presentation.state.HomeUiState
+import com.example.home.presentation.state.HomeUiEvent
 import com.example.home.presentation.state.Resource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,29 +15,28 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getUserUseCase: GetUserUseCase,
-    /*savedStateHandle: SavedStateHandle*/
 ) : ViewModel() {
 
-    private val _state: MutableStateFlow<HomeUiState?> = MutableStateFlow(null)
-    val state: StateFlow<HomeUiState?> = _state
+    private val _state: MutableStateFlow<HomeUiEvent?> = MutableStateFlow(null)
+    val state: StateFlow<HomeUiEvent?> = _state
 
     init {
         fetchUsers()
     }
 
-    fun fetchUsers() {
+    private fun fetchUsers() {
         viewModelScope.launch {
             when (val result = getUserUseCase.invoke()) {
                 is Resource.Loading -> {
-                    HomeUiState.Loading(result.loadingStatus)
+                    HomeUiEvent.Loading(result.loadingStatus)
                 }
 
                 is Resource.Success -> {
-                    HomeUiState.Success(result.data)
+                    HomeUiEvent.Success(result.data)
                 }
 
                 is Resource.Failure -> {
-                    HomeUiState.Failure(result.message)
+                    HomeUiEvent.Failure(result.message)
                 }
             }.also {
                 _state.value = it
